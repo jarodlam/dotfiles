7bbe39ddbc901cf4b47c60af55698b2f96faf9ec
Jarod Lam <10664255+jarodlam@users.noreply.github.com>
2025-07-11 14:43:57 +1000

    catppuccin

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "frappe",
+        transparent_background = true,
+        default_integrations = true,
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      theme = "catppuccin",
+      options = {
+        component_separators = " ",
+        section_separators = { left = "", right = "" },
+      },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    optional = true,
+    opts = {
+      highlights = require("catppuccin.groups.integrations.bufferline").get(),
+      options = {
+        separator_style = "slant",
+      },
+    },
+  },
+}
