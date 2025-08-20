f42a98ab187a0b37f0b36ce6762b8295fac81c57
Jarod Lam <10664255+jarodlam@users.noreply.github.com>
2025-08-20 15:39:43 +1000

    fix catppuccin

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -4,13 +4,11 @@ return {
     lazy = false,
     name = "catppuccin",
     priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "frappe",
-        transparent_background = true,
-        default_integrations = true,
-      })
-    end,
+    opts = {
+      flavour = "frappe",
+      transparent_background = true,
+      -- default_integrations = true,
+    },
   },
   {
     "LazyVim/LazyVim",
@@ -31,8 +29,17 @@ return {
   {
     "akinsho/bufferline.nvim",
     optional = true,
+
+    -- temp fix https://github.com/LazyVim/LazyVim/pull/6354
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+
     opts = {
-      highlights = require("catppuccin.groups.integrations.bufferline").get(),
+      highlights = require("catppuccin.groups.integrations.bufferline").get_theme(),
       options = {
         separator_style = "thin",
       },
